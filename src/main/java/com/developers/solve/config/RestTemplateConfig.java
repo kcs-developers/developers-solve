fad175c870820741d9d30a33fa9fa4a879bc9ce0
lango <xmun777@naver.com>
2023-03-19 23:25:38 +0900

    feat: RestTemplate Exception Hander apply

--- src/main/java/com/developers/solve/config/RestTemplateConfig.java
+++ src/main/java/com/developers/solve/config/RestTemplateConfig.java
@@ -1,13 +1,21 @@
 package com.developers.solve.config;
 
+import com.developers.solve.exception.RestTemplateResponseErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+/**
+ * restTemplate()
+ * RestTemplate 빈을 등록해두고 커스텀 예외 클래스를 주입받아서 예외 처리 진행
+ */
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
+        return restTemplate;
     }
 }
