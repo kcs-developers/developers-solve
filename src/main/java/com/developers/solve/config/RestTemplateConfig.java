0634c148cac0a37fa3efb87b905950b6fced76cc
lango <xmun777@naver.com>
2023-03-19 23:50:15 +0900

    fix: RestTemplate Exception Hander to RestTemplateBuilder fix

--- src/main/java/com/developers/solve/config/RestTemplateConfig.java
+++ src/main/java/com/developers/solve/config/RestTemplateConfig.java
@@ -1,9 +1,13 @@
 package com.developers.solve.config;
 
 import com.developers.solve.exception.RestTemplateResponseErrorHandler;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 /**
  * restTemplate()
@@ -13,9 +17,11 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new RestTemplateResponseErrorHandler());
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .errorHandler(new RestTemplateResponseErrorHandler())
+                .build();
     }
 }
