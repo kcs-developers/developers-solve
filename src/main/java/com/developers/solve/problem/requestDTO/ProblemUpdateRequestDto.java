250cee6f3f20dbb1076f062ff220e590bbfa3bb0
EagerProgrammer <yth960617@unist.ac.kr>
2023-04-10 20:18:49 +0900

    Feat : Exception Handler 작성완료 및 Validation 처리 완료

--- src/main/java/com/developers/solve/problem/requestDTO/ProblemUpdateRequestDto.java
+++ src/main/java/com/developers/solve/problem/requestDTO/ProblemUpdateRequestDto.java
@@ -2,6 +2,7 @@ package com.developers.solve.problem.requestDTO;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +13,11 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class ProblemUpdateRequestDto {
-    @NotBlank
+    @NotNull(message = "수정 권한이 없습니다.")
     private Long problemId;
     @Nullable
     private String type;
-    @NotBlank
+    @NotBlank(message = "수정 권한이 없습니다.")
     private String writer;
     @Nullable
     private String title;
